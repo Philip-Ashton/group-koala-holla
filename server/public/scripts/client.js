4fab0e1c73e39703badf76278101ff241cf7922d
Philip Ashton <philligan1@gmail.com>
2024-05-16 19:12:33 -0500

    first hour day 1

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,13 +3,47 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  axios.get('/koalas')
+    .then((response) => {
+        const koalas = response.data;
+        const contentDiv = document.querySelector('#viewKoalas');
+        contentDiv.innerHTML = '';
+        let contentHTML = '';    
+
+        for (let koala of koalas){
+          contentHTML += `
+          <tr>
+            <td>${koala.name}</td>
+            <td>${koala.age}</td>
+            <td>${koala.favoriteColor}</td>
+            <td>${koala.readyToTransfer}</td>
+            <td>${koala.notes}</td>
+            <td><button>Ready for transfer</button></td>
+            <td><button>Delete</button></td>
+          </tr>
+          `;
+        }
+        contentDiv.innerHTML = contentHTML;
+      })
+      .catch((error)=>{
+      console.log(error);
+      alert('Get Koala went wrong');
+      });
   
 } // end getKoalas
+
+
 
 function saveKoala(){
   console.log( 'in saveKoala' );
   // axios call to server to get koalas
- 
+axios.post('/koalas', viewKoala)
+.then ((respoonse) => {
+  document.querySelector('#addKoalaInput').value = '';
+})
+.catch((error) => {
+  console.log(error);
+});
 }
 
 getKoalas();
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,17 +2,38 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+let koalaList = [
+    { id: 1, name: 'Scotty', favoriteColor: 'Red', age: 4, readyToTransfer: true, notes: 'Born in Guatemala'},
+    { id: 2, name: 'Jean', favoriteColor: 'Green', age: 5, readyToTransfer: true, notes: 'Allergic to lots of lava'},
+    { id: 3, name: 'Ororo', favoriteColor: 'Yellow', age: 7, readyToTransfer: false, notes: 'Loves listening to Paula (Abdul)'},
+    { id: 4, name: "K'Leaf", favoriteColor: 'Purple', age: 15, readyToTransfer: false, notes: 'Never refueses a treat.'},
+    { id: 5, name: 'Charlie', favoriteColor: 'Orange', age: 9, readyToTransfer: true, notes: 'Favorite band is Nirvana'},
+    { id: 6, name: 'Betsy', favoriteColor: 'Blue', age: 4, readyToTransfer: true, notes: 'Has a pet iguana'},
+]
 
 // GET
-
+koalaRouter.get('/', function (request, response){
+    response.send(koalaList);
+});
 
 // POST
-
+koalaRouter.post('/', function (request, response){
+    const newKoala = request.body;
+    newKoala.id = koalaList.length + 1;
+    koalaList.push(newKoala);
+    response.sendStatus(201);
+})
 
 // PUT
-
+// koalaRouter.put('/', function (request,response){
+//     .then(response =>)
+// })
 
 // DELETE
+koalaRouter.delete('/:id', function (request, response){
+    const koalaId = Number(request.params.id);
+    koalaList = koalaList.filter(k => k.id !== koalaId);
+    response.sendStatus(201);
+});
 
 module.exports = koalaRouter;
