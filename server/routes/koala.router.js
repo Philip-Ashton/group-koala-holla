1b054f49c9a74a05c436b2f400a81fc6582c8aba
Philip Ashton <philligan1@gmail.com>
2024-05-23 19:03:20 -0500

    updated routerJS

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,7 +4,7 @@ const koalaRouter = express.Router();
 const pg = require('pg');
 // DB CONNECTION
 const pool = new pg.Pool({
-    database: 'koalas',
+    database: 'Koala Holla',
     host: 'localhost',
     port: 5432
 });
@@ -27,12 +27,34 @@ koalaRouter.get('/', function (request, response){
 // POST
 koalaRouter.post('/', function (request, response){
     console.log('request.body', request.body);
-    
+
     let newKoala = request.body;
+
+    let name = newKoala.name;
+    let age = newKoala.age;
+    let favoriteColor = newKoala.favoriteColor;
+    let readyToTransfer = newKoala.readyToTransfer;
+    let notes = newKoala.notes;
+
+    let queryText = `INSERT INTO "koalas" ("name", "age", "favoriteColor", "readyToTransfer", "notes")
+    VALUE ($1, $2, $3, $4, $5);`
     //newKoala.id = koalaList.length + 1;
     //koalaList.push(newKoala);
     //response.sendStatus(201);
-})
+    pool.query(queryText, [name, age, favoriteColor, readyToTransfer, notes])
+        .then(dbResult => {
+            console.log('dbResult.rows', dbResult.rows);
+            response.sendStatus(201);
+        })
+        .catch(dbError => {
+            // unpack the results
+            console.log('dbError:', dbError);
+            // send the client a response, based on the results.
+            response.sendStatus(500);
+
+        console.log('Adding koalas', newKoala);
+        })
+});
 
 // PUT
 // koalaRouter.put('/', function (request,response){
@@ -41,9 +63,27 @@ koalaRouter.post('/', function (request, response){
 
 // DELETE
 koalaRouter.delete('/:id', function (request, response){
-    const koalaId = Number(request.params.id);
-    koalaList = koalaList.filter(k => k.id !== koalaId);
-    response.sendStatus(201);
+    //const koalaId = Number(request.params.id);
+    //koalaList = koalaList.filter(k => k.id !== koalaId);
+    //response.sendStatus(201);
+    console.log('req.params', request.params);
+
+    let idToDelete = request.params.id
+
+    console.log('idToDelete', idToDelete);
+    console.log('typeof idToDelete', typeof idToDelete);
+
+    let queryText = `DELETE FROM "koalas" WHERE id = $1;`;
+
+    pool.query(queryText, [idToDelete])
+        .then(dbResult => {
+            console.log(dbResult);
+            response.sendStatus(200);
+        })
+        .catch(dbError => {
+            console.log(dbError);
+            response.sendStatus(500);
+        })
 });
 
 module.exports = koalaRouter;
